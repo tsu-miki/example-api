ad19ad2e925e8e7fe6e581058d54419b26e62b84
tsu-miki <wimps.r0930@icloud.com>
2026-03-18 23:23:40 +0900

    add systems ping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/v1/systems/ping", get(|| async { "pong" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
