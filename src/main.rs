2225af9edeb8ad61604b3e4968b1620241515697
tsu-miki <wimps.r0930@icloud.com>
2026-03-18 23:40:28 +0900

    add handler,driver

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use axum::{Router, routing::get};
+use handler::handler::ping;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/v1/systems/ping", get(|| async { "pong" }));
+    let app = Router::new()
+        .route("/v1/systems/ping", get(|| ping()))
+        .route("/v1/companies", get(|| async { "companies" }));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
