a87999cb3a8b594e094ad5864b4c4a60c0d765d7
tsu-miki <wimps.r0930@icloud.com>
2026-03-21 12:23:36 +0900

    add field

--- handler/src/handler.rs
+++ handler/src/handler.rs
@@ -10,6 +10,9 @@ pub async fn ping() -> &'static str {
 #[derive(Serialize, new, Debug)]
 pub struct CompanyJson {
     id: String,
+    name: String,
+    founded_year: i32,
+    employee_count: i32
 }
 
 type CompaniesJson = Vec<CompanyJson>;
@@ -18,7 +21,14 @@ pub async fn get_companies() -> Json<CompaniesJson> {
     let companies_data = get_companies_data().await;
     let companies_json = companies_data
         .iter()
-        .map(|company| CompanyJson::new(company.id.to_string()))
+        .map(|company| {
+            CompanyJson::new(
+                company.id.to_string(),
+                company.name.to_string(),
+                company.founded_year,
+                company.employee_count
+            )
+        })
         .collect::<CompaniesJson>();
     Json(companies_json)
 }
